918dd9d9f909970d5cbbcd694cd56b159bf4c201
Marlo Morales <marlomorales@Marlos-MBP.lan>
2022-07-28 15:52:13 -0700

    add update scalarwaves with constraints eqs and pdf lectur

--- gabe_lectures/scalar_wave_system_lecture/gabes_three_advection_scalarwave__eqs.py
+++ gabe_lectures/scalar_wave_system_lecture/gabes_three_advection_scalarwave__eqs.py
@@ -49,22 +49,46 @@ def updated_psi(psi, pi, phi, delta_t):
     return psi + -pi * delta_t
 
 
-def updated_pi_LF(psi, pi, phi, delta_t):
+def constraint_error(psi, phi, xs):
+    # phi should be equal to the spatial derivative of psi at all times
+    return faster_finite_differentiation(xs, psi) - phi
+
+
+def updated_psi_second_order_in_time(psi, pi, phi, delta_t, previous_psi):
+    return 1.0 / 3.0 * (-2.0 * pi * delta_t + 4.0 * psi - previous_psi)
+
+
+def updated_pi_LF(psi, pi, phi, delta_t, apply_BCs=False):
     dphi_dx = faster_finite_differentiation(xs, phi)
-    new_pi = [pi[0] - dphi_dx[0] * delta_t]
+    if not apply_BCs:
+        new_pi = [pi[0] - dphi_dx[0] * delta_t]
+    if apply_BCs:
+        new_pi = [pi[0]]
     for i in range(1, len(psi) - 1):
         new_pi.append(0.5 * (pi[i - 1] + pi[i + 1]) - dphi_dx[i] * delta_t)
-    new_pi.append(pi[-1] - dphi_dx[-1] * delta_t)
+    if not apply_BCs:
+        new_pi.append(pi[-1] - dphi_dx[-1] * delta_t)
+    if apply_BCs:
+        new_pi.append(pi[0])
     return np.array(new_pi)
 
 
-def updated_phi_LF(psi, pi, phi, delta_t):
+def updated_phi_LF(psi, pi, phi, delta_t, damping=False, apply_BCs=False):
+    gamma_2 = 0.01
     dpi_dx = faster_finite_differentiation(xs, pi)
     new_phi = [phi[0] - dpi_dx[0] * delta_t]
+    if apply_BCs:
+        new_phi = [phi[1]]
     for i in range(1, len(psi) - 1):
         new_phi.append(0.5 * (phi[i - 1] + phi[i + 1]) - dpi_dx[i] * delta_t)
-    new_phi.append(phi[-1] - dpi_dx[-1] * delta_t)
-    return np.array(new_phi)
+    if not apply_BCs:
+        new_phi.append(phi[-1] - dpi_dx[-1] * delta_t)
+    if apply_BCs:
+        new_phi.append(-phi[1])
+    if damping == False:
+        return np.array(new_phi)
+    if damping == True:
+        return np.array(new_phi) + gamma_2 * constraint_error(psi, phi, xs)
 
 
 ## Set up discretized fields:
@@ -73,6 +97,7 @@ xs = np.linspace(-5, 15, N)
 psis = initial_wave_profile_psi(xs)
 pis = initial_wave_profile_pi(xs)
 phis = initial_wave_profile_phi(xs)
+constraints = constraint_error(psis, phis, xs)
 delta_x = xs[1] - xs[0]
 v = 1.0
 delta_t = 0.1
@@ -92,6 +117,9 @@ ax.set_ylim(-0.75, 0.75)
 line1, = ax.plot([])
 line2, = ax.plot([])
 line3, = ax.plot([])
+line4, = ax.plot([])
+line5, = ax.plot([])
+line6, = ax.plot([])
 
 psis_all = [psis]
 pis_all = [pis]
@@ -99,17 +127,18 @@ phis_all = [phis]
 temp_psis = [psis]
 temp_pis = [pis]
 temp_phis = [phis]
+constraints_all = [constraints]
 
 
 def animate(frame_num):
-    #psis = updated_psi(xs, psis, v, delta_t)
-    #line.set_data((xs, psis))
-    damping = False
+    # set damping function to True to see the damping effect
+    damping = True
     apply_BCs = True
     temp_psis[0] = updated_psi(psis_all[0], pis_all[0], phis_all[0], delta_t)
-    temp_pis[0] = updated_pi_LF(psis_all[0], pis_all[0], phis_all[0], delta_t)
+    temp_pis[0] = updated_pi_LF(psis_all[0], pis_all[0], phis_all[0], delta_t,
+                                apply_BCs)
     temp_phis[0] = updated_phi_LF(psis_all[0], pis_all[0], phis_all[0],
-                                  delta_t)
+                                  delta_t, damping, apply_BCs)
     psis_all[0] = temp_psis[0]
     pis_all[0] = temp_pis[0]
     phis_all[0] = temp_phis[0]
@@ -118,12 +147,15 @@ def animate(frame_num):
     line1.set_data((xs, psis_all[0]))
     line2.set_data((xs, pis_all[0]))
     line3.set_data((xs, phis_all[0]))
-    return [line1, line2, line3]
+    line4.set_data((xs, constraints_all[0]))
+    line5.set_data((xs, pis_all[0] + phis_all[0]))
+    line6.set_data((xs, pis_all[0] - phis_all[0]))
+    return [line1, line2, line3, line4, line5, line6]
 
 
 anim = animation.FuncAnimation(fig,
                                animate,
                                frames=100,
-                               interval=150,
+                               interval=50,
                                blit=True)
 plt.show()
